1014abe3df3285b2c43b6bc2f0511ab8315b66e3
unknown <maykedesenvolvimento@gmail.com>
2020-11-11 09:32:26 -0300

    Rota GET criada

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const transactionRouter = express.Router();
+const Transaction = require('../models/TransactionModel')
+
+transactionRouter.get('/', async (req, res) => {
+    const period = (req.query.period) ? req.query.period.split('-') : null;
+
+    if (!period || period.length<2) res.status(400).json({message: 'É necessário informar o período no formato YYYY-MM!'})
+
+    const year = period[0];
+    const month = period[1];
+
+    const ts = await Transaction.find({year, month});
+
+    res.send(ts);
+})
 
 module.exports = transactionRouter;
